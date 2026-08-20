62dfa0d4bdaf3bfc1ef1474e4d2f79f9f5d1fcfe
Nilay <42715700+nilaypranjale@users.noreply.github.com>
2026-08-20 11:49:44 +0530

    Add: three-integer-sum - submission-22

---
+++ Data Structures & Algorithms/three-integer-sum/submission-22.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+//HASHMAP
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> res = new ArrayList();
+        Map<Integer, Integer> hm = new HashMap();
+
+        for(int n : nums)
+        {
+            hm.put(n, hm.getOrDefault(n,0)+1);
+        }
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            hm.put(nums[i],hm.get(nums[i])-1);
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            for(int j = i + 1; j < nums.length; j++)
+            {
+                hm.put(nums[j],hm.get(nums[j])-1);    
+                if(j>i+1 && nums[j]==nums[j-1]) continue; //j>i+1; coz j=i is ok
+                int t = -(nums[i]+nums[j]);
+                if(hm.getOrDefault(t,0)>0)
+                {
+                    res.add(Arrays.asList(nums[i],nums[j],t));
+                }
+            }
+            for(int j = i+1; j < nums.length; j++)
+            {
+                hm.put(nums[j],hm.get(nums[j])+1);
+            }
+        }
+        return res;
+    }
+}
